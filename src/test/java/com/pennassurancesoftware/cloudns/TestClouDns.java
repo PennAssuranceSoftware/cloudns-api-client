97030e1a84ae0ee21de0398d917d8a1cc5f38db5
jeromebridge <jeromebridge@pennassurancesoftware.com>
2015-02-17 17:40:09 -0500

    Started POC to call ClouDNS

---
+++ src/test/java/com/pennassurancesoftware/cloudns/TestClouDns.java
@@ -0,0 +1,11 @@
+package com.pennassurancesoftware.cloudns;
+
+import org.testng.annotations.Test;
+
+public class TestClouDns {
+
+   @Test(groups = { "integration" }, enabled = true)
+   public void testCreateCnameRecord() throws Exception {
+      
+   }
+}
